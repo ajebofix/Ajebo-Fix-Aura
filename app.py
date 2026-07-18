62024b5ea87a703af54b452274864e78a7b502bf
ajebofix <ajebofix@gmail.com>
2026-07-18 16:41:54 +0100

    Avoid public bind by default

--- app.py
+++ app.py
@@ -209,7 +209,7 @@ app = create_app()
 
 if __name__ == "__main__":
     app.run(
-        host="0.0.0.0",
+        host=os.getenv("HOST", "127.0.0.1"),
         port=int(os.getenv("PORT", "5000")),
         debug=os.getenv("FLASK_DEBUG") == "1",
     )
